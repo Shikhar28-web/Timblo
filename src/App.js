e2d54c2c7a444ce7b50cddf0800f891c7a5a2194
Shikhar Verma <vermashikhar380@gmail.com>
2025-06-03 21:09:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
 
 
 import './HomePage.css';
